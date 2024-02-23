1ca6bb8416ad46fe1286e9dfc409483c666be53d
romulosilveira <150481220+romulosilveira@users.noreply.github.com>
2024-02-23 12:09:24 -0300

    Add files via upload

---
+++ prova1_logica.py
@@ -0,0 +1,13 @@
+#TÓPICO 1
+nome = "Frederico Galdino"
+idade = 9
+peso = 47.3
+vacinado = True
+print(type(nome), type(idade), type(peso), type(vacinado))
+
+#TÓPICO 2
+nome_aluno = str(input("Digite o seu nome completo: "))
+idade_aluno = int(input("Digite sua idade: "))
+
+#TÓPICO 3
+print(f"O aluno {nome_aluno} possui {idade_aluno} anos.")
